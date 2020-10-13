fa3a41e78713670733262fd60f85af99a611741d
Timur Mukhtarov <Ummalife7@gmail.com>
2020-10-13 07:08:15 +0600

    Отправка на код ревью. V2

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-let profileItemsEdit = document.querySelector(".profile-items__edit");
+let profileItemsEdit = document.querySelector(".profile__edit");
 let popupClose = document.querySelector(".popup__close");
 let popup = document.querySelector(".popup");
 let popupSave = document.querySelector(".popup__save");
-let profileItemsName = document.querySelector(".profile-items__name");
-let profileInfoStatus = document.querySelector(".profile-info__status");
-let popupName = document.querySelector(".popup__name");
-let popupStatus = document.querySelector(".popup__status");
+let profileItemsName = document.querySelector(".profile__name");
+let profileInfoStatus = document.querySelector(".profile__status");
+let popupName = document.querySelector(".popup__item_who_name");
+let popupStatus = document.querySelector(".popup__item_who_status");
 
 
 function popupToggle() {
@@ -18,8 +18,3 @@ function popupToggle() {
 profileItemsEdit.addEventListener("click", popupToggle);
 popupClose.addEventListener("click", popupToggle);
 popupSave.addEventListener("click", popupToggle);
-
-let elementItemName = document.querySelector(".element-item__name");
-function nameString() {
-    elementItemName
-}
